013fd92707d769f3ff5ff60423563f71165ca1d1
DerEddie <e.krutitsky@yahoo.com>
2021-08-06 10:37:33 +0200

    #test comment

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -3,7 +3,7 @@ import mysql.connector
 cnx = mysql.connector.connect(user='root', password='jajaveve',
                               host='127.0.0.1',
                               database='store')
-
+#test
 mycursor = cnx.cursor()
 
 mycursor.execute("SELECT * FROM orders")
