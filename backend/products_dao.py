d31a66132a3b5d831108bad81f0f897cda49b9b8
DerEddie <e.krutitsky@yahoo.com>
2021-08-06 10:54:22 +0200

    Merge

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -6,7 +6,7 @@ cnx = mysql.connector.connect(user='root', password='jajaveve',
 #test
 mycursor = cnx.cursor()
 
-mycursor.execute("SELECT * FROM orders")
+mycursor.execute("SELECT products.product_id, products.name, products.uom_id, products.price_per_unit FROM products inner join uom on products.uom_id = uom.uom_id")
 
 myresult = mycursor.fetchall()
 
