9c9eced077d201e24f35524992871df343eefbf9
Tim <82032094+TimS27@users.noreply.github.com>
2021-08-06 09:19:14 +0200

    Create products_dao.py

---
+++ backend/products_dao.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(user='root', password='password1',
+                              host='127.0.0.1',
+
+                              database='store')
+
+cursor = cnx.cursor()   #cursor will hold data from sql query
+
+query = "SELECT * FROM store.products;"
+
+cursor.execute(query)
+
+for (product_id, name, uom_id, price_per_unit) in cursor:
+    print(product_id, name, uom_id, price_per_unit)
+
+cnx.close()
