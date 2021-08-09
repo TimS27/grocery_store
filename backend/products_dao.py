d7626388d91b7118d5f24351c0e849671edf9cf1
Tim Schönwald <timschoenwald@googlemail.com>
2021-08-09 08:58:26 +0200

    -added array that is returned to get_all_products -wrote initial server.py

--- backend/products_dao.py
+++ backend/products_dao.py
@@ -1,24 +1,30 @@
 import mysql.connector
 from sql_connection import get_sql_connection
+
+
 def get_all_products(cnx):
     cnx.reconnect()
-    #test
     mycursor = cnx.cursor()
     query1 = "SELECT products.product_id, products.name, products.uom_id, products.price_per_unit, uom.uom_name FROM products inner join uom on products.uom_id = uom.uom_id"
     mycursor.execute(query1)
     myresult = mycursor.fetchall()
 
-    print("query")
+    response = []
+
+    #print("query")
     for x in myresult:
-      print(x)
+        response.append(x)
+        #print(x)
     cnx.close()
+    return response
+
 
 def insert_new_product(cnx, dict_):
     cnx.reconnect()
     mycursor = cnx.cursor()
     query2 = ("INSERT INTO products (name, uom_id, price_per_unit) VALUES (%s,%s,%s)")
-    data =(dict_['product_name'],dict_['uom_id'], dict_['price_per_unit'])
-    q= query2 % data
+    data = (dict_['product_name'], dict_['uom_id'], dict_['price_per_unit'])
+    q = query2 % data
     print(q)
     mycursor.execute(query2, data)
     # Commits Database Change
@@ -27,23 +33,23 @@ def insert_new_product(cnx, dict_):
 
     print("inserted")
     for x in myresult:
-      print(x)
+        print(x)
     cnx.close()
+
 
 def delete_product(cnx, product_id):
     cnx.reconnect()
     cursor = cnx.cursor()
-    query = ("DELETE FROM products where product_id=" +str(product_id))
+    query = ("DELETE FROM products where product_id=" + str(product_id))
     cursor.execute(query)
     connection.commit()
     cnx.close()
 
+
 if __name__ == '__main__':
     connection = get_sql_connection()
-
-
     dict_ = {'product_name': 'potato', 'uom_id': '1', 'price_per_unit': 10}
-    insert_new_product(connection, dict_)
-    delete_product(connection, 4)
+    #insert_new_product(connection, dict_)
+    delete_product(connection, 9)
 
     get_all_products(connection)
---
+++ backend/server.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+import products_dao
+from sql_connection import get_sql_connection
+
+app = Flask(__name__)
+
+connection = get_sql_connection()
+
+# @app.route('/hello')
+# def hello():
+#    return "Hello, how are you?"
+
+
+@app.route('/get_products', methods=['GET'])
+def get_products():
+    products = products_dao.get_all_products(connection)
+    print(products)
+    response = jsonify(products)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Store Management System")
+    app.run(port=5000)
